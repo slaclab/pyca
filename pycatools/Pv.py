a01f6ecc97f4dc81722c88db6ce16e82cfaceebb
Michael C Browne <mcbrowne@slac.stanford.edu>
2012-07-24 17:47:59 +0000

    Changed put to raise exception on channel access error.

--- pycatools/Pv.py
+++ pycatools/Pv.py
@@ -7,12 +7,19 @@ class Pv(pyca.capv):
   def __init__(self, name):
     pyca.capv.__init__(self, name)
     self.__connection_sem = threading.Event()
+    self.__putevt_sem = threading.Event()
     self.connect_cb = self.connection_handler
+    self.putevt_cb = self.putevt_handler
+    self.__putevt_exc = None
 
   # Channel access callbacks
   def connection_handler(self, isconnected):
     if isconnected:
       self.__connection_sem.set()
+
+  def putevt_handler(self, e=None):
+    self.__putevt_exc = e
+    self.__putevt_sem.set()
 
   # Calls to channel access methods
   def connect(self, timeout=-1.0):
@@ -36,9 +43,22 @@ class Pv(pyca.capv):
     tmo = float(timeout)
     self.get_data(ctrl, tmo)
 
+  #
+  # Two simultaneous puts from different threads won't work with this code.
+  # If we want to support this, we need to serialize puts using a lock.
+  #
   def put(self, value, timeout=-1.0):
     tmo = float(timeout)
-    self.put_data(value, tmo)
+    self.__putevt_exc = None
+    self.__putevt_sem.clear()
+    self.put_data(value, -1.0)
+    pyca.flush_io()
+    if tmo > 0:
+      self.__putevt_sem.wait(tmo)
+      if not self.__putevt_sem.isSet():
+        raise pyca.pyexc, "put timedout for PV %s" %(self.name)
+      elif self.__putevt_exc != None:
+        raise pyca.caexc, self.__putevt_exc
     
   # Used to obtain a copy of the data which won't be overwritten by ca callbacks
   def getcopy(self):
