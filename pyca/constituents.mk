94049563176642a92119ccbb3ee543f9b0a6b52a
Amedeo Perazzo <perazzo@slac.stanford.edu>
2013-08-26 21:15:03 +0000

    Use python 2.7 only

--- pyca/constituents.mk
+++ pyca/constituents.mk
@@ -1,8 +1,7 @@
 libnames := pyca
 
 libsrcs_pyca := pyca.cc
-libincs_pyca := python/include/python2.5 \
-		python/include/python2.7 \
+libincs_pyca := python/include/python2.7 \
                 epics/include \
                 epics/include/os/Linux
 liblibs_pyca := epics/ca \
