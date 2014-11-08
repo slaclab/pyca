9c177c9bd7519f8af2025aba4681dc1e5360dd54
Daniel Flath <dflath@slac.stanford.edu>
2014-11-08 03:34:40 +0000

    bump version to 2.1.0 in anticipation of release

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ pyca = Extension('pyca', language = 'c++', sources = ['pyca/pyca.cc'],
                  library_dirs=libdirs,
                  libraries=['Com', 'ca'])
 
-setup(name='pyca', version='2.0.1',
+setup(name='pyca', version='2.1.0',
       description='python channel access library',
-      ext_modules=[pyca], url='http://slac.stanford.edu',
+      ext_modules=[pyca], url='https://confluence.slac.stanford.edu/display/PCDS/Using+pyca',
       author='Amedeo Perazzo', author_email='amedeo@slac.stanford.edu')
 
