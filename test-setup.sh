f8c595687f39a6ff2a6366dc15e89265b4297702
Zachary Lentz <zlentz@slac.stanford.edu>
2017-11-14 15:19:21 -0800

    BLD: Rename test dirs to avoid pytest conflicts

--- test-setup.sh
+++ test-setup.sh
@@ -1,6 +1,6 @@
-mkdir -p setup-py-test
+mkdir -p setup-py-tst
 python setup.py build
 LIB=`find build -name pyca.so`
-LINK='setup-py-test/pyca.so'
+LINK='setup-py-tst/pyca.so'
 rm $LINK
 ln -s $LIB $LINK
