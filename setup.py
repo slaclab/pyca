57ce7f259dc8fc48ffa84ecff5fdc32834f482f9
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-05 13:50:59 -0800

    BLD: clean up setup.py and add psp package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ elif sys.platform.startswith('linux'):
     libsrc = 'Linux'
 else:
     libsrc = None
-    
+
 epics_inc = os.getenv("EPICS_BASE") + "/include"
 epics_lib = os.getenv("EPICS_BASE") + "/lib/" + os.getenv("EPICS_HOST_ARCH")
 numpy_inc = np.get_include()
@@ -21,7 +21,9 @@ pyca = Extension('pyca',
                  library_dirs=[epics_lib],
                  libraries=['Com', 'ca'])
 
-setup(name='pyca', version='3.0.0',
+setup(name='pyca',
+      version='3.0.0',
       description='python channel access library',
-      ext_modules=[pyca], url='https://confluence.slac.stanford.edu/display/PCDS/Using+pyca',
-      author='Amedeo Perazzo', author_email='amedeo@slac.stanford.edu')
+      packages=['psp'],
+      ext_modules=[pyca],
+     )
