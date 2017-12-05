7752374f66be536c384b8463998b611ed58e31fc
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-04 19:15:00 -0800

    TST: Simplify test build and do not link if build fails

--- test-setup.sh
+++ test-setup.sh
@@ -1,6 +1,8 @@
-mkdir -p setup-py-tst
 python setup.py build
 LIB=`find build -name pyca.so`
-LINK='setup-py-tst/pyca.so'
+LINK='pyca.so'
 rm $LINK
+if [ -z $LIB ]; then
+  exit
+fi
 ln -s $LIB $LINK
